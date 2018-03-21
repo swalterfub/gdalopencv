873dedd9fec6d963b2f49d1a289761f88fbc1db7
Sebastian Walter <s.walter@fu-berlin.de>
2018-03-21 16:19:42 +0100

    minor

--- gdalToOpenCV.cpp
+++ gdalToOpenCV.cpp
@@ -11,7 +11,7 @@
 // OpenCV Dependencies
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
